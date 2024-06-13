17466bf4db79a01c28424a35dfc3742141e94809
Richard Staszkiewicz <137425623+RichardStaszkiewicz@users.noreply.github.com>
2024-06-13 21:23:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,17 +60,26 @@ module "vertex_ai_workbench" {
 }
 
 #
+#module "dataproc" {
+#  depends_on   = [module.vpc]
+#  source       = "./modules/dataproc"
+#  project_name = var.project_name
+#  region       = var.region
+#  subnet       = module.vpc.subnets[local.notebook_subnet_id].id
+#  # machine_type = "e2-standard-2"
+#  machine_type                    = var.dataproc_machine_type
+#  worker_nodes_number             = var.dataproc_num_workers
+#  worker_nodes_number_preemptible = var.dataproc_workers_preemptible
+#  no_worker_nodes                 = var.no_worker_nodes
+#}
+
 module "dataproc" {
   depends_on   = [module.vpc]
-  source       = "./modules/dataproc"
+  source       = "github.com/bdg-tbd/tbd-workshop-1.git?ref=v1.0.36/modules/dataproc"
   project_name = var.project_name
   region       = var.region
   subnet       = module.vpc.subnets[local.notebook_subnet_id].id
-  # machine_type = "e2-standard-2"
-  machine_type                    = var.dataproc_machine_type
-  worker_nodes_number             = var.dataproc_num_workers
-  worker_nodes_number_preemptible = var.dataproc_workers_preemptible
-  no_worker_nodes                 = var.no_worker_nodes
+  machine_type = "e2-standard-4"
 }
 
 ## Uncomment for Dataproc batches (serverless)
